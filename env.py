733f7b14230bc458255023681bb860c22c1652a0
Nickyfrs <nickyfrs@gmail.com>
2022-07-09 13:24:01 +0100

    created the basic file structure and the starting code for the app.py fiel and the env.py file. started from cero

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "NftRlbZHRMKWRtrXu8Me9ZrW5YnbXOCp")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://nickyfrs:Cherlina10@cluster0.rqlwn.mongodb.net/?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")  
